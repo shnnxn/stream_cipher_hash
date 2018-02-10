693daf0f26ecd1a37d2423d3a4965416db6086b2
shnnxn <fmtalkk@gmail.com>
2018-02-09 16:06:23 -0800

    stream cipher completed

--- stream_cipher.py
+++ stream_cipher.py
@@ -1,13 +1,22 @@
-from operator import xor
 import binascii
 
-def string_to_binary():
-  st = "hello world"
-  key = "10101010"
-  er = [ bin(ord(ch))[2:].zfill(8) for ch in st ] #in list form
-  pt = ' '.join(format(ord(x), 'b') for x in st)    # not in list form
-  z = int(er[1], 2)^int(key, 2)
-  enc = bin(z)[2:].zfill(len(er[1]))
-  return enc
 
-print (string_to_binary())
+def user_text():
+  plaintext = "hello i am stream cipher"
+  string_to_binary(plaintext)
+
+def string_to_binary(user_text):
+  binary_list = [ bin(ord(ch))[2:].zfill(8) for ch in user_text ]         #in list form
+  import pdb;pdb.set_trace()
+  encryption_box(binary_list)
+
+def encryption_box(binary_list):
+  key = "01101011"
+  ct = []
+  for i in range(len(binary_list)):
+    xor_key_blist = int(binary_list[i], 2)^int(key, 2)                      #xored
+    encrypting = bin(xor_key_blist)[2:].zfill(len(binary_list[1]))    #xored encryption
+    ct.append(encrypting)
+  import pdb;pdb.set_trace()
+
+print (user_text())
