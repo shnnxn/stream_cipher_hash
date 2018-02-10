f779048ae7f91fa4a8f880af7612eff9c512b138
shnnxn <fmtalkk@gmail.com>
2018-02-09 18:23:01 -0800

    stream cipher final

--- stream_cipher.py
+++ stream_cipher.py
@@ -1,22 +1,25 @@
 import binascii
 
-
 def user_text():
   plaintext = "hello i am stream cipher"
+  print plaintext
   string_to_binary(plaintext)
 
 def string_to_binary(user_text):
-  binary_list = [ bin(ord(ch))[2:].zfill(8) for ch in user_text ]         #in list form
-  import pdb;pdb.set_trace()
+  binary_list = [ bin(ord(ch))[2:].zfill(8) for ch in user_text ] #in list form
   encryption_box(binary_list)
 
 def encryption_box(binary_list):
   key = "01101011"
+  ct_blist = []
+  ct_dec = []
   ct = []
   for i in range(len(binary_list)):
-    xor_key_blist = int(binary_list[i], 2)^int(key, 2)                      #xored
+    xor_key_blist = int(binary_list[i], 2)^int(key, 2) #xored
     encrypting = bin(xor_key_blist)[2:].zfill(len(binary_list[1]))    #xored encryption
-    ct.append(encrypting)
-  import pdb;pdb.set_trace()
+    ct_blist.append(encrypting)
+    ct_dec.append(int(ct_blist[i], 2))
+    ct.append(chr(ct_dec[i]))
+  print("cipher text : ")
+  print ct
 
-print (user_text())
